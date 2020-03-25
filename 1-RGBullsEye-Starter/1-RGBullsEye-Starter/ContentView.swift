66d780ffc9d20d7e4ef67150cb671856a0496946
le-xuan-quynh <le-xuan-quynh@goodcycle.net>
2020-03-26 08:12:27 +0900

    add new project

---
+++ 1-RGBullsEye-Starter/1-RGBullsEye-Starter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  1-RGBullsEye-Starter
+//
+//  Created by Le Xuan Quynh on 2020/03/26.
+//  Copyright © 2020 Le Xuan Quynh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
